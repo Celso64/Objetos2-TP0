ba864b7b8823e7b687c788e149ab77c1ad1506ab
carlos <carlos@carlos-System-Product-Name>
2023-03-15 00:25:09 -0300

    Trastie con Java Funcional

--- restaurante/src/main/java/modelo/Viedma.java
+++ restaurante/src/main/java/modelo/Viedma.java
@@ -1,17 +1,22 @@
 package modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Viedma implements Tarjeta {
 
 	@Override
 	public Double pagarCuenta(Cuenta cuenta, Integer propina) {
-		List<Producto> lista = cuenta.getValores();
+
+		List<Double> lista = new ArrayList<>();
+
+		cuenta.getValores().forEach(x -> lista.add((double) x.valor()));
+
 		Double precio = 0.0;
-		for (Producto p : lista) {
-			precio += p.valor();
-		}
+
+		precio = lista.stream().reduce(0.0, (x, y) -> x + y);
 		precio *= (1 + propina / 100.0);
+
 		return precio;
 	}
 
